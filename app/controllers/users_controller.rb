730e2e4c0a5276067bee83637933641dfa8a67e8
wec410sin <wec410sin@yahoo.co.jp>
2021-02-13 22:08:47 +0900

    railstask2

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,37 @@
+class UsersController < ApplicationController
+# 以下のアクションを追加
+ def index
+   @users = User.all
+ end
+
+ def new
+  @user = User.new
+ end
+
+ def create
+   # Storong Parameter の記述
+   User.create(user_params)
+   redirect_to action: :index
+ end
+
+ def edit
+   @user = User.find(params[:id])
+ end
+
+ def update
+   user = User.find(params[:id])
+   user.update(user_params)
+   redirect_to action: :index
+ end
+
+ def destroy
+   user = User.find(params[:id])
+   user.destroy
+   redirect_to action: :index
+ end
+
+ private
+ def user_params
+   params.require(:user).permit(:name, :age)
+ end
+end
