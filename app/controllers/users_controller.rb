b4cbe96c05179bbeca0711b2bef67162a3f366b2
wec410sin <wec410sin@yahoo.co.jp>
2021-02-15 22:26:48 +0900

    resourcesを使用して修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
    redirect_to action: :index
  end
 
+
  def edit
    @user = User.find(params[:id])
  end
@@ -23,6 +24,7 @@ class UsersController < ApplicationController
    user.update(user_params)
    redirect_to action: :index
  end
+
 
  def destroy
    user = User.find(params[:id])
